6bce21d74a85245249b337ed588b4026c716eb59
Robert Hamilton <robert@rghsoftware.com>
2025-09-21 17:17:42 -0500

    Update mongdb config to check for env var before falling back to local connection

--- config/mongodbconn.cjs
+++ config/mongodbconn.cjs
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const uri = "mongodb://localhost:27017/yogidb"
+const uri = process.env.MONGO_URI || "mongodb://localhost:27017/yogidb"
 
 mongoose.connect(uri)
 .then(() => console.log("✅ Connected to MongoDB"))
